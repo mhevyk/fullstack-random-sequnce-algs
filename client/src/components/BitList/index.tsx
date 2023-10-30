2a218ef1fe654c7b5fdb075d24a09e405828d62c
mhevyk <maxxevyk7@gmail.com>
2023-10-30 17:36:48 +0200

    Added react-window library for optimising BitList component

--- client/src/components/BitList/index.tsx
+++ client/src/components/BitList/index.tsx
@@ -1,26 +1,58 @@
-import { memo } from "react";
+import { CSSProperties } from "react";
+import { FixedSizeList } from "react-window";
+import AutoSizer from "react-virtualized-auto-sizer";
+
+const SCROLLBAR_HEIGHT = 15;
+const GAP_X = 4;
+const GAP_Y = 16;
+const ITEM_SIZE = 35;
 
 type BitListItemProps = {
-  bit: number;
+  index: number;
+  style: CSSProperties;
+  data: number[];
 };
 
-const BitListItem = memo(({ bit }: BitListItemProps) => {
-  return <div className={`bit bit_${bit}`}>{bit}</div>;
-});
+const BitListItem = ({ index, style, data }: BitListItemProps) => {
+  const bit = data[index];
+
+  return (
+    <div
+      style={{ ...style, width: ITEM_SIZE, height: ITEM_SIZE }}
+      className={`bit bit_${bit}`}
+    >
+      {bit}
+    </div>
+  );
+};
 
 type BitListProps = {
   data: number[];
 };
 
-export const BitList = memo(({ data }: BitListProps) => {
+export const BitList = ({ data }: BitListProps) => {
   return (
     <>
-      Згенерована наступна послідовність з {data.length} елементів:{" "}
-      <div className="d-flex overflow-x-auto gap-1 mb-2 py-3">
-        {data.map((bit, index) => (
-          <BitListItem key={index} bit={bit} />
-        ))}
+      Згенерована наступна послідовність з {data.length} елементів:
+      <div
+        className="my-3"
+        style={{ height: ITEM_SIZE + SCROLLBAR_HEIGHT + GAP_Y }}
+      >
+        <AutoSizer>
+          {({ width, height }) => (
+            <FixedSizeList
+              height={height}
+              itemCount={data.length}
+              itemData={data}
+              itemSize={ITEM_SIZE + GAP_X}
+              width={width}
+              layout="horizontal"
+            >
+              {BitListItem}
+            </FixedSizeList>
+          )}
+        </AutoSizer>
       </div>
     </>
   );
-});
+};
